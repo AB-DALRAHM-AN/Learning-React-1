b1834d348c8c58acd0f465aa37f453dfe951ade0
ABDALRHMAN <abdalrhmanahmed00004@gmail.com>
2023-10-07 18:17:31 +0300

    session one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name = "Abdo";
+  function readName(name) {
+    if (name) {
+      return <h1>Hello, {name}</h1>;
+    } else {
+      return <h1>Hello, Stranger</h1>;
+    }
+  }
+  return <div className="App">
+    <h1>Hello, {name}</h1>
+    {readName(name)}
+  </div>;
 }
 
 export default App;
