f3a9e7ee6c2989299d1c036800ecfd4157eb6cf2
ABDALRHMAN <abdalrhmanahmed00004@gmail.com>
2023-10-07 18:30:37 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  const name = "Abdo";
+  const name = "abdo";
   function readName(name) {
     if (name) {
       return <h1>Hello, {name}</h1>;
@@ -9,10 +9,16 @@ function App() {
       return <h1>Hello, Stranger</h1>;
     }
   }
-  return <div className="App">
-    <h1>Hello, {name}</h1>
-    {readName(name)}
-  </div>;
+  return (
+    <>
+    <div className="App">
+      {readName(name)}
+    </div>
+    <div className="text">
+      <h1>Hello, {name}</h1>
+    </div>
+    </>
+  );
 }
 
 export default App;
